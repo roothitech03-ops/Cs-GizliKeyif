86696ba13ed14c913002bcefc0d8822ee0b22400
roothitech03-ops <roothitech03@gmail.com>
2026-03-03 17:23:04 +0530

    Add files via upload

---
+++ LatestLeaks/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("Manus AI")
+    language    = "en"
+    description = "Latest Leaked Porn - New Porn Videos Streaming or Download full and free"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://latestleaks.co/wp-content/uploads/2023/08/cropped-favicon-32x32.png"
+}
