e5b76333c6fd28ab3c70aaa746fc5f9127be2e11
roothitech03-ops <roothitech03@gmail.com>
2026-03-03 20:58:30 +0530

    Add files via upload

---
+++ LatestLeaks/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("kerimmkirac")
+    language    = "en"
+    description = "Latest Leaked Porn - New Porn Videos Streaming or Download full and free"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=latestleaks.co&sz=%size%"
+}
