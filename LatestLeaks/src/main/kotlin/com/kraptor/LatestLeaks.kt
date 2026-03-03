295d7ce3b82ea888a60e93b475f9b6fc19dfe135
roothitech03-ops <roothitech03@gmail.com>
2026-03-03 17:50:24 +0530

    Update LatestLeaks.kt

--- LatestLeaks/src/main/kotlin/com/kraptor/LatestLeaks.kt
+++ LatestLeaks/src/main/kotlin/com/kraptor/LatestLeaks.kt
@@ -1,18 +1,10 @@
 package com.kraptor
 
-import android.content.Context
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.cloudstream3.utils.fixUrlNull
 import org.jsoup.nodes.Element
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.extractors.Streamtape
 
 class LatestLeaks : MainAPI() {
     override var mainUrl              = "https://latestleaks.co"
@@ -39,11 +31,11 @@ class LatestLeaks : MainAPI() {
 
     private fun Element.toMainPageResult(): SearchResponse? {
         val title     = this.selectFirst("h2.entry-title a")?.text() ?: return null
-        val href      = fixUrlNull(this.selectFirst("h2.entry-title a")?.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
+        val href      = this.selectFirst("h2.entry-title a")?.attr("href") ?: return null
+        val posterUrl = this.selectFirst("img")?.attr("data-src") ?: this.selectFirst("img")?.attr("src")
 
-        return newMovieSearchResponse(title, href, TvType.NSFW) {
-            this.posterUrl = posterUrl
+        return newMovieSearchResponse(title, fixUrl(href), TvType.NSFW) {
+            this.posterUrl = fixUrl(posterUrl ?: "")
         }
     }
 
@@ -58,17 +50,14 @@ class LatestLeaks : MainAPI() {
         val document = app.get(url).document
 
         val title           = document.selectFirst("h1.entry-title")?.text()?.trim() ?: return null
-        val poster          = fixUrlNull(document.selectFirst("meta[property=og:image]")?.attr("content"))
+        val poster          = document.selectFirst("meta[property=og:image]")?.attr("content")
         val description     = document.selectFirst("div.entry-content p")?.text()?.trim()
         val tags            = document.select("p.post-tags a").map { it.text() }
 
-        val iframeSrc = document.selectFirst("iframe")?.attr("src")
-
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
-            this.posterUrl       = poster
+            this.posterUrl       = fixUrl(poster ?: "")
             this.plot            = description
             this.tags            = tags
-            // No direct year, score, duration, recommendations, actors, trailer from page
         }
     }
 
@@ -77,10 +66,8 @@ class LatestLeaks : MainAPI() {
         val iframeSrc = document.selectFirst("iframe")?.attr("src")
 
         if (iframeSrc != null) {
-            if (iframeSrc.contains("streamtape.com")) {
-                Streamtape().call(iframeSrc, data, subtitleCallback, callback)
-            }
-            // Add other extractors here if needed
+            val fixedIframeSrc = fixUrl(iframeSrc)
+            loadExtractor(fixedIframeSrc, data, subtitleCallback, callback)
         }
         return true
     }
