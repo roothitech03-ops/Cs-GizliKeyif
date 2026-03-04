da3b4312cc1b5f111b7874e19a940fe6a07920a9
roothitech03-ops <roothitech03@gmail.com>
2026-03-04 09:29:51 +0530

    Add files via upload

---
+++ PornHD4K/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("kerimmkirac")
+    language    = "en"
+    description = "Watch Free Porn Videos in 4K HD ⭐️ Full Length!"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.pornhd4k.net&sz=%size%"
+}
