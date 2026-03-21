ecd8247a9cc41d3da153c83f27c7836afc5f7ff0
roothitech03-ops <roothitech03@gmail.com>
2026-03-21 22:14:59 +0530

    Update build.gradle.kts

--- watchporn/build.gradle.kts
+++ watchporn/build.gradle.kts
@@ -1,12 +1,7 @@
 cloudstream {
     authors = listOf("kraptor")
-    
-    tvTypes = listOf("NSFW")
-    
     iconUrl = "https://watchporn.to/favicon.ico"
-    
-    description = "High quality adult streams from WatchPorn.to - Taboo & Step-family content"
-    
-    version = 1
-    // apiVersion = 20
+    description = "WatchPorn.to - Adult Streams"
+    tvTypes = listOf("NSFW")
+    lang = "en"
 }
