72e1b59b27f48b6c0dfeebabaffb34fe54af1649
roothitech03-ops <roothitech03@gmail.com>
2026-03-21 22:15:48 +0530

    Add imports for LoadResponse and MainApi classes

--- watchporn/src/main/kotlin/com/kraptor/WatchPornPlugin.kt
+++ watchporn/src/main/kotlin/com/kraptor/WatchPornPlugin.kt
@@ -1,6 +1,12 @@
 package com.kraptor
 
-import com.lagradost.cloudstream3.BasePlugin
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.MainApi
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.MainPageResponse
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.providers.BasePlugin
 
 class WatchPornPlugin : BasePlugin() {
     override fun load() {
