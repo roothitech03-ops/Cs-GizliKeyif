7ba12a54001d10af23e2bf1dcfede1f0c357f486
roothitech03-ops <roothitech03@gmail.com>
2026-03-21 21:45:59 +0530

    Add cloudstream configuration to build.gradle.kts
    Added configuration for cloudstream with authors, tvTypes, iconUrl, description, version, and apiVersion.

---
+++ watchporn/build.gradle.kts
@@ -0,0 +1,12 @@
+cloudstream {
+    authors = listOf("kraptor")
+    
+    tvTypes = listOf("NSFW")
+    
+    iconUrl = "https://watchporn.to/favicon.ico"
+    
+    description = "High quality adult streams from WatchPorn.to - Taboo & Step-family content"
+    
+    version = 1
+    apiVersion = 20
+}
