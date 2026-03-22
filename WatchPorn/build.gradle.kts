27d0f0595956131cea45b7b2e785f3a29028721f
roothitech03-ops <roothitech03@gmail.com>
2026-03-22 23:50:02 +0530

    Add files via upload

---
+++ WatchPorn/build.gradle.kts
@@ -0,0 +1,19 @@
+// ! Bu araç @Kraptor123 tarafından | @Cs-GizliKeyif için yazılmıştır.
+version = 3
+
+cloudstream {
+    authors     = listOf("kraptor")
+    language    = "en"
+    description = "WatchPorn.to is a free adult streaming platform featuring taboo, step-family, MILF, and exclusive content in Full HD quality."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://watchporn.to/&size=128"
+}
