8caf47c3cd26564e2a697e0a5a1b1c8052141c1d
roothitech03-ops <roothitech03@gmail.com>
2026-03-22 19:49:30 +0530

    Delete watchporn directory

--- watchporn/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-cloudstream {
-    authors = listOf("kraptor")
-    iconUrl = "https://watchporn.to/favicon.ico"
-    description = "WatchPorn.to - Adult Streams"
-    tvTypes = listOf("NSFW")
-    lang = "en"
-}
