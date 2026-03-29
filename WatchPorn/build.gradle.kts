08e5d145399ceb8e2de5eff4d0b599fd78b0dcb2
roothitech03-ops <roothitech03@gmail.com>
2026-03-30 00:19:19 +0530

    Add files via upload

---
+++ WatchPorn/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("kerimmkirac")
+    language    = "en"
+    description = "Watch Free Porn Videos in 8K HD ⭐️ Full Length!"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=en16.pornhd8k.net&sz=%size%"
+}
