97db0eb6b3cb7e1bceb37931bd5f93b34c7b0291
roothitech03-ops <roothitech03@gmail.com>
2026-04-05 17:22:17 +0530

    Create ToonWorld4All/src/main/kotlin/com/kraptor/ToonWorld4AllPlugin.kt

---
+++ ToonWorld4All/src/main/kotlin/com/kraptor/ToonWorld4AllPlugin.kt
@@ -0,0 +1,12 @@
+package com.kraptor
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class ToonWorld4AllPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(ToonWorld4AllProvider())
+    }
+}
